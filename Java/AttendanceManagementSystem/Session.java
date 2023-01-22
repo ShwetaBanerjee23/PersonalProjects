e1162efca73795bfcfeddd3a973f2b859678cd4b
Shweta Banerjee <shwetabanerjee23@gmail.com>
2023-01-22 18:36:41 +0000

    Create Session.java

---
+++ Java/AttendanceManagementSystem/Session.java
@@ -0,0 +1,13 @@
+/* An enum representing all the possible sessions. */
+public enum Session {
+    MONDAY_AM,
+    MONDAY_PM,
+    TUESDAY_AM,
+    TUESDAY_PM,
+    WEDNESDAY_AM,
+    WEDNESDAY_PM,
+    THURSDAY_AM,
+    THURSDAY_PM,
+    FRIDAY_AM,
+    FRIDAY_PM
+}
